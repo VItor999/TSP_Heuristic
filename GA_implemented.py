4a5308e9c3838ea52a2852bd7d971340ae612cda
VCARV_WSL_UBUNTU <vcarvalho999@gmail.com>
2024-10-13 16:45:17 -0300

    GA: Adding documentation
    Signed-off-by: VCARV_WSL_UBUNTU <vcarvalho999@gmail.com>

--- GA_implemented.py
+++ GA_implemented.py
@@ -1,21 +1,45 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import random
 from tsp_utils.general import *
 
 def create_route(num_cities):
-    """Creates a random tour (route) of the cities."""
+    '''
+    Creates a random tour (route) of the cities.
+
+    :param num_cities: number of cities to visit
+    :type num_cities: int 
+    :return: list with a random tour of the cities 
+    :rtype: list[int]
+    '''
+    
     route = list(range(num_cities))
     random.shuffle(route)
-    #print(route)
     return route
 
 def initial_population(num_cities, population_size):
-    """Generates the initial population of routes."""
+    '''
+    Generates the initial population of routes.
+
+    :param num_cities: number of cities to visit
+    :type num_cities: int
+    :param population_size: Number of beings in the population
+    :type population_size: int
+    :return: list containing the route for each member of the population
+    :rtype: list[list[int]]
+    '''
+    
     return [create_route(num_cities) for _ in range(population_size)]
 
 def route_distance(route, distance_matix):
-    """Calculates the total distance of a route."""
+    '''
+    _summary_
+
+    :param route: Tour; route to have total distance calculated
+    :type route: list[int]
+    :param distance_matix: Matrix with the distance form point to point
+    :type distance_matix: dict
+    :return: total distance of a route
+    :rtype: float
+    '''
+    
     distance = 0.0
     num_cities = len(route)
     for i in range(num_cities):
@@ -24,17 +48,50 @@ def route_distance(route, distance_matix):
         distance += distance_matix[from_city][to_city]
     return distance
 
+
 def fitness(route, distance_matrix):
-    """Calculates the fitness of a route based on inverse of distance."""
+    '''
+    Calculates the fitness of a route based on inverse of distance
+
+    :param route: tour to be executed
+    :type route: list[int]
+    :param distance_matix: Matrix with the distance form point to point
+    :type distance_matix: dict
+    :return: metric to indicate how good is the route (the grater the better)
+    :rtype: float
+    '''
+    
     return 1.0 / route_distance(route, distance_matrix)
 
 def rank_routes(population, distance_matrix):
-    """Ranks routes in the population based on fitness."""
+    '''
+    Ranks routes in the population based on fitness
+
+    :param population: current population
+    :type population: list[list[int]]
+    :param distance_matix: Matrix with the distance form point to point
+    :type distance_matix: dict
+    :return: population resorted based on best route 
+    :rtype: list[list[int]]
+    '''
+    
     fitness_results = [(route, fitness(route, distance_matrix)) for route in population]
     return sorted(fitness_results, key=lambda x: x[1], reverse=True)
 
 def selection(pop_ranked, elite_size = 5, tournament_size = 5):
-    """Selects parent routes using tournament selection."""
+    '''
+    Selects parent routes using tournament selection
+
+    :param pop_ranked: ranked population 
+    :type pop_ranked: list[list[int]]
+    :param elite_size: Size of the elite of the population, defaults to 5
+    :type elite_size: int, optional
+    :param tournament_size: Size of the tournament/number of elements selected to find the best to breed, defaults to 5
+    :type tournament_size: int, optional
+    :return: elements selected to breed
+    :rtype: list[list[int]]
+    '''
+    
     selection_results = []
     for elite in range(elite_size):
         selection_results.append(pop_ranked[elite][0])
@@ -45,7 +102,17 @@ def selection(pop_ranked, elite_size = 5, tournament_size = 5):
     return selection_results
 
 def breed(parent1, parent2):
-    """Performs Order Crossover (OX) to create a child route."""
+    '''
+    Breeds two parents to generate a new elements.Performs Order Crossover (OX) to create a child route
+
+    :param parent1: First parent
+    :type parent1: list[int]
+    :param parent2: Second parent
+    :type parent2: list[int]
+    :return: Newly generate child
+    :rtype: list[int]
+    '''
+    
     num_cities = len(parent1)
     child = [None] * num_cities
 
@@ -70,7 +137,16 @@ def breed(parent1, parent2):
     return child
 
 def breed_population(mating_pool, elite_size = 5):
-    """Creates a new population through crossover."""
+    '''
+    Creates a new population through crossover.
+
+    :param mating_pool: Selected elements to breed 
+    :type mating_pool: list[list[int]]
+    :param elite_size: Elite size/elements that will be copied directly, defaults to 5
+    :type elite_size: int, optional
+    :return: new part of the population
+    :rtype: list[list[int]]
+    '''
     children = []
     length = len(mating_pool)
     pool = random.sample(mating_pool, len(mating_pool))
@@ -84,7 +160,17 @@ def breed_population(mating_pool, elite_size = 5):
     return children
 
 def mutate(route, mutation_rate=0.01):
-    """Performs swap mutation on a route."""
+    '''
+    Performs swap mutation on a route
+
+    :param route: one element of the population
+    :type route: list[int]
+    :param mutation_rate: possibility of mutation, defaults to 0.01 (1%)
+    :type mutation_rate: float, optional
+    :return: mutated route/individual
+    :rtype: list[int]
+    '''
+ 
     num_cites = len(route)
     for swapped in range(num_cites):
         if random.random() < mutation_rate:
@@ -98,7 +184,17 @@ def mutate(route, mutation_rate=0.01):
     return route
 
 def mutate_population(population, mutation_rate=0.01 ):
-    """Applies mutation to the population."""
+    '''
+    Applies mutation to the population.
+
+    :param population: Un-mutated population
+    :type population: list[list[int]]
+    :param mutation_rate: possibility of mutation, defaults to 0.01 (1%)
+    :type mutation_rate: float, optional
+    :return: Mutated population 
+    :rtype: list[list[int]]
+    '''
+    
     mutated_pop = []
     for ind in range(len(population)):
         mutated_ind = mutate(population[ind],mutation_rate)
@@ -106,7 +202,23 @@ def mutate_population(population, mutation_rate=0.01 ):
     return mutated_pop
 
 def next_generation(current_gen, distance_matrix,mutation_rate = 0.01, tournament_size = 5, elite_size = 5):
-    """Creates the next generation."""
+    '''
+    Creates the next generation.
+
+    :param current_gen: Current population
+    :type current_gen: list[list[int]]
+    :param distance_matix: Matrix with the distance form point to point
+    :type distance_matix: dict
+    :param mutation_rate: _description_, defaults to 0.01
+    :type mutation_rate: float, optional
+    :param tournament_size: Size of the tournament/number of elements selected to find the best to breed, defaults to 5
+    :type tournament_size: int, optional
+    :param elite_size: Elite size/elements that will be copied directly, defaults to 5
+    :type elite_size: int, optional
+    :return: new generation 
+    :rtype: list[list[int]]
+    '''
+    
     pop_ranked = rank_routes(current_gen, distance_matrix)
     selection_results = selection(pop_ranked, elite_size, tournament_size)
     mating_pool = selection_results
