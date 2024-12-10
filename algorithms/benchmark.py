968b7a3605e230269cd76135cd0e35ceded8e667
Vitor Almeida de Carvalho <vcarvalho999@gmail.com>
2024-12-09 21:01:45 -0300

    Benchmark: Making benchmark use a TS algorithm and fixing bug induced by making distance matrixcontain floats
    Signed-off-by: Vitor Almeida de Carvalho <vcarvalho999@gmail.com>

--- algorithms/benchmark.py
+++ algorithms/benchmark.py
@@ -55,23 +55,23 @@ def print_solution(manager, routing, solution):
     plan_output += f" {manager.IndexToNode(index)}\n"
     print(plan_output)
 
-def benchmark(locations = None, points_num = 4, shape = 'square'):
+def benchmark(locations=None, points_num=4, shape='square'):
     '''
-    Solves the problem using the default/ out of the box  solution form google OR
+    Solves the problem using the Tabu Search algorithm in Google OR-Tools.
 
-    :param points: set of points to be used, defaults to None
-    :type points: list of (x,y) tuples, optional
-    :param points_num: number of points to generate, defaults to 10
+    :param locations: Set of points to be used, defaults to None
+    :type locations: list of (x, y) tuples, optional
+    :param points_num: Number of points to generate, defaults to 10
     :type points_num: int, optional
-    :param shape: string the defines the shape to be generated, defaults to square
+    :param shape: String defining the shape to be generated, defaults to square
     :type shape: str, optional
     :return: solution found, else None
-    :rtype: (RoutingIndexManager, RoutingModel, Assignment, data_model_model) objects from google OR and custom data_model_model
+    :rtype: (RoutingIndexManager, RoutingModel, Assignment, data_model_model) objects from Google OR-Tools and custom data_model_model
     '''
     # Instantiate the data_model problem.
-    if(locations == None):
+    if locations is None:
         locations = generate_form_points(points_num, shape)
-        
+
     data_model = create_data_model(locations)
 
     # Create the routing index manager.
@@ -82,29 +82,48 @@ def benchmark(locations = None, points_num = 4, shape = 'square'):
     # Create Routing Model.
     routing = pywrapcp.RoutingModel(manager)
 
-    distance_matrix = compute_euclidean_distance_matrix(data_model["locations"])
+    # Compute the distance matrix.
+    float_distance_matrix = compute_euclidean_distance_matrix(data_model["locations"])
+    # Scale up the distance matrix to integers.
+    scaled_distance_matrix = {
+        from_node: {
+            to_node: int(distance * 10000)
+            for to_node, distance in row.items()
+        }
+        for from_node, row in float_distance_matrix.items()
+    }
 
     def distance_callback(from_index, to_index):
-        """Returns the distance between the two nodes."""
+        """Returns the scaled distance between the two nodes."""
         # Convert from routing variable Index to distance matrix NodeIndex.
         from_node = manager.IndexToNode(from_index)
         to_node = manager.IndexToNode(to_index)
-        return distance_matrix[from_node][to_node]
+        return scaled_distance_matrix[from_node][to_node]
 
     transit_callback_index = routing.RegisterTransitCallback(distance_callback)
 
     # Define cost of each arc.
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
-    # Setting first solution heuristic.
+    # Setting Tabu Search parameters.
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.first_solution_strategy = (
-        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+    search_parameters.local_search_metaheuristic = (
+        routing_enums_pb2.LocalSearchMetaheuristic.TABU_SEARCH
     )
+    search_parameters.time_limit.seconds = 2 # time limit for the search.
 
     # Solve the problem.
     solution = routing.SolveWithParameters(search_parameters)
-    return (manager, routing, solution, data_model) if solution else None
+
+    if solution:
+        # Rescale distances in the solution.
+        total_distance = float(solution.ObjectiveValue()) / 10000.0
+        print(f"Total distance (rescaled): {total_distance}")
+        return manager, routing, solution, data_model
+    else:
+        return None
+
+
 
 if __name__ == "__main__":
     solution = benchmark(points_num = 20, shape = 'square')
